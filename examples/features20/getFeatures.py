236ba32db700cb1603c2ec0f2f2af73ba4adca81
myay <myayla1245@gmail.com>
2018-06-25 14:40:23 +0200

    extend getFeatures script with timing and DT function

--- examples/features20/getFeatures.py
+++ examples/features20/getFeatures.py
@@ -3,10 +3,28 @@ from scipy import misc
 import numpy as np
 from skimage import io
 
+import time
+import cProfile as profile
+
 import featuresWVT
 import featuresFFT
 
-features = []
-featuresFFT.fftfeatures(features)
-featuresWVT.wvtfeatures(features)
-print(features)
+def timeFeatures():
+    profile.runctx("getFeatures()", globals(), locals())
+
+def decisionTree(F):
+
+    return True
+
+def getFeatures():
+    features = []
+    featuresFFT.fftfeatures(features)
+    featuresWVT.wvtfeatures(features)
+    concatFeatures = np.concatenate((features[0], features[1]), axis=0)
+    print(concatFeatures)
+
+    decisionTree(concatFeatures)
+
+if __name__=="__main__":
+    #timeFeatures()
+    getFeatures()
