754bb8aac07e6012158015460a57bedc6c23cbea
myay <myayla1245@gmail.com>
2018-07-07 12:21:12 +0200

    generate srad indices lookup table

--- examples/features20/genInd/generateSpec.py
+++ examples/features20/genInd/generateSpec.py
@@ -3,6 +3,7 @@ import numpy as np
 rmax = 32-1
 x0 = 32
 y0 = 32
+width = 32
 m_pi_f = 3.14159265358979323846
 
 m = 0
@@ -13,10 +14,14 @@ def main():
 
     CcodeYc = "int sradyc[{rmax}][180] = {".replace("{rmax}", str(rmax+1))
 
-    # generate indices for srad up to 64 x 64
+    CcodeInd = "int xc_yc[{rmax}][180] = {".replace("{rmax}", str(rmax+1))
+
+    # generate indices for srad up for 64 x 64
     #print(m)
     xc = np.zeros(shape=(rmax+1, 180))
     yc = np.zeros(shape=(rmax+1, 180))
+
+    xc_yc = np.zeros(shape=(rmax+1, 180))
 
     for r in range(2, rmax+1):
 
@@ -24,15 +29,19 @@ def main():
         if r == 2:
             CcodeXc += "{"
             CcodeYc += "{"
+            CcodeInd += "{"
             for i in range(2):
                 for j in range(180):
                     CcodeXc += "0,"
                     CcodeYc += "0,"
+                    CcodeInd += "0,"
                     if j == 179:
                         CcodeXc = CcodeXc[:-1]
                         CcodeYc = CcodeYc[:-1]
+                        CcodeInd = CcodeInd[:-1]
                 CcodeXc += "}, {"
                 CcodeYc += "}, {"
+                CcodeInd += "}, {"
 
             #if i == 1:
             #    CcodeXc = CcodeXc[:-2]
@@ -40,6 +49,7 @@ def main():
         else:
             CcodeXc += "{"
             CcodeYc += "{"
+            CcodeInd += "{"
 
 
         for angle in range (91,271):
@@ -52,19 +62,26 @@ def main():
 
             CcodeYc += "{yc},".replace("{yc}", str(int(yc[r][array_index])))
 
+            CcodeInd += "{ind},".replace("{ind}", str(int(yc[r][array_index])*width + int(xc[r][array_index])))
+
+            print(int(xc[r][array_index]), int(yc[r][array_index]))
+
             if array_index == 179:
                 CcodeXc = CcodeXc[:-1]
                 CcodeYc = CcodeYc[:-1]
+                CcodeInd = CcodeInd[:-1]
 
         CcodeXc += "}, "
         CcodeYc += "}, "
+        CcodeInd += "}, "
         if r == rmax:
             CcodeXc = CcodeXc[:-2]
             CcodeYc = CcodeYc[:-2]
+            CcodeInd = CcodeInd[:-2]
 
     CcodeXc += "};"
     CcodeYc += "};"
-
+    CcodeInd += "};"
 
     '''for i in range(180):
         print(i)
@@ -73,8 +90,9 @@ def main():
             print(xc[180*i + j], yc[180*i +j])
         print("next")'''
 
-    print(CcodeXc)
-    print(CcodeYc)
+    #print(CcodeXc)
+    #print(CcodeYc)
+    #print(CcodeInd)
 
 if __name__=="__main__":
     main()
