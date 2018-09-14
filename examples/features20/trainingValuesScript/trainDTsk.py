d98ff5a13411a01349451776f21977e64a502ada
myay <myayla1245@gmail.com>
2018-09-15 00:07:28 +0200

    split training and test data to measure classification performance

--- examples/features20/trainingValuesScript/trainDTsk.py
+++ examples/features20/trainingValuesScript/trainDTsk.py
@@ -2,7 +2,9 @@ from sklearn import tree
 import numpy as np
 import re
 import copy
-
+import graphviz
+from sklearn.model_selection import cross_val_predict, KFold, train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
 def trainDT():
@@ -20,9 +22,31 @@ def trainDT():
     ones9672 = np.full((9672), 1, dtype=int)
     Y = np.concatenate((zeros9674, ones9672), axis=0)
 
-    clf = tree.DecisionTreeClassifier()
-    clf.fit(X, Y)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=3422, )
 
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(X_train, y_train)
+
+    pred = cross_val_predict(clf, X_test, y_test, cv=5)
+    print('Mean of CV:  {:.4%}'.format(accuracy_score(pred, y_test)))
+
+    dt_param = 0.9
+
+    score_train = accuracy_score(clf.predict(X_train) > dt_param, y_train)
+    decision_function = clf.predict(X_test)
+    pred = decision_function > dt_param
+    score = accuracy_score(y_test, pred)
+    precision = precision_score(y_test, pred)
+    recall = recall_score(y_test, pred)
+
+    print('Train score: {:.4%}'.format(score_train))
+    print('Test score:  {:.4%}'.format(score))
+    print('Precision:   {:.4%}'.format(precision))
+    print('Recall:      {:.4%}'.format(recall))
+
+    dot_data  = tree.export_graphviz(clf, out_file=None, class_names=['0','1'])
+    graph = graphviz.Source(dot_data)
+    graph.render("iris")
     print('Training completed.')
 
 def getData(examples):
