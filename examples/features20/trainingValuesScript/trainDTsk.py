e8a3c1bbd8f7bacb50ced69fb54eceb78d02be3c
myay <myayla1245@gmail.com>
2018-09-17 01:18:41 +0200

    pca plot

--- examples/features20/trainingValuesScript/trainDTsk.py
+++ examples/features20/trainingValuesScript/trainDTsk.py
@@ -6,6 +6,8 @@ import copy
 import graphviz
 from sklearn.model_selection import cross_val_predict, KFold, train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+import matplotlib.pyplot as plt
+from sklearn.decomposition.pca import PCA
 
 class DecisionTree:
     def __init__(self):
@@ -45,16 +47,24 @@ def trainDT():
     precision = precision_score(y_test, pred)
     recall = recall_score(y_test, pred)
 
+    # print classification performance metrics
     print('Train score: {:.4%}'.format(score_train))
     print('Test score:  {:.4%}'.format(score))
     print('Precision:   {:.4%}'.format(precision))
     print('Recall:      {:.4%}'.format(recall))
 
+    # visualize tree
     dot_data  = tree.export_graphviz(clf, out_file=None, class_names=['0','1'])
-    #graph = graphviz.Source(dot_data)
-    #graph.render("iris")
+    graph = graphviz.Source(dot_data)
+    graph.render("iris")
+
+    # extract tree code in python from sklearn
     tree_to_code(clf, ['x0','x1','x2','x3','x4','x5','x6','x7','x8','x9','x10','x11','x12','x13','x14','x15','x16','x17','x18','x19'])
 
+    # show pca curve
+    pca(clf, X_train, X_test, y_train, y_test, pred)
+
+    # store trained model
     file = open("skTree.py","w")
     file.write(DT.DTcode)
     file.close()
@@ -124,6 +134,35 @@ def tree_to_code(tree, feature_names):
             DT.DTcode += ("\n{}return {}".format(indent, np.argmax(tree_.value[node][0])))
 
     recurse(0, 1)
+
+def pca(model, X_train, X_test, y_train, y_test, pred, threshold=0, weights_train=None, weights_test=None):
+    pca_model = PCA(2)
+    X_train_pca = pca_model.fit_transform(X_train)
+    X_test_pca = pca_model.transform(X_test)
+
+    plt.subplot(131)
+    plt.suptitle('PCA')
+    plt.title('Training data')
+    plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1], cmap=plt.cm.bwr_r, c=y_train,
+                alpha=0.9)
+    plt.axis('on')
+    plt.subplot(132)
+    plt.title('Test data')
+    plt.scatter(X_test_pca[:, 0], X_test_pca[:, 1], cmap=plt.cm.bwr_r, c=y_test,
+                alpha=0.9)
+    plt.axis('on')
+    plt.subplot(133)
+    plt.title('Prediction')
+    xx, yy = np.meshgrid(np.linspace(np.min(X_test_pca[:, 0]), np.max(X_test_pca[:, 0]), 50),
+                         np.linspace(np.min(X_test_pca[:, 1]), np.max(X_test_pca[:, 1]), 50))
+    Z = model.predict(pca_model.inverse_transform(np.c_[xx.ravel(), yy.ravel()])) - threshold
+    plt.contourf(xx, yy, Z.reshape(xx.shape), 50, alpha=0.5, cmap=plt.cm.bwr_r, vmin=-1, vmax=1)
+    plt.scatter(X_test_pca[:, 0], X_test_pca[:, 1], cmap=plt.cm.bwr_r, c=pred,
+                alpha=0.9)
+    plt.axis('on')
+    plt.savefig('pca.png')
+    plt.show()
+
 
 if __name__=="__main__":
     trainDT()
