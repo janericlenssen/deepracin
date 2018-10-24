ebedd2571bb06f67f5d398ae9fff4eef0f9ac31a
myay <myayla1245@gmail.com>
2018-10-24 22:13:58 +0200

    run time script

--- examples/comparison/measureRunTime.sh
+++ examples/comparison/measureRunTime.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-pyScript="featuresFFT.py"
+#pyScript="featuresFFT.py"
 #pyScript="featuresWVT.py"
-#pyScript="build_dr_model.py"
+pyScript="build_dr_model.py"
 
 counter=1
 sum=0
-while [ $counter -le 10 ]
+while [ $counter -le 1000 ]
 do
-    sum=$(bc -l <<< "$sum + $(python ${pyScript} | grep -oP '(?<=Whole graph took: )[\d.]+')")
+    term=$(python ${pyScript} | grep -oP '(?<=Whole graph took: )[\d.]+')
+    echo "$term" | bc -l
+    sum=$(bc -l <<< "$sum + $term")
     ((counter++))
 done
-echo "$sum / 10" | bc -l
+echo "****DONE****"
+echo "Average:"
+echo "$sum / 1000" | bc -l
